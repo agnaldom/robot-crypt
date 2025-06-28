1e5b4b1186244561686851fb1c6b92abf74d0949
agnaldo.marinho.cev <agnaldo.marinho.cev@elo.com.br>
2025-06-28 19:08:41 -0300

    Remove arquivo nao usados

--- debug_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Export environment variables
-export $(cat .env | grep -v '^#' | xargs)
-
-# Print them
-echo "TELEGRAM_BOT_TOKEN = '$TELEGRAM_BOT_TOKEN'"
-echo "TELEGRAM_CHAT_ID = '$TELEGRAM_CHAT_ID'"
