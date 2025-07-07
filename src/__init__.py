21575537345e07a9cc6ae4be13d26c91a9a1211e
agnaldo.marinho.cev <agnaldo.marinho.cev@elo.com.br>
2025-07-07 16:07:38 -0300

    Atualiza src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -10,66 +10,66 @@ __author__ = "Robot-Crypt Team"
 
 # Principais módulos do projeto
 try:
-    from .core.config import Config
+    from src.core.config import Config
 except ImportError:
     Config = None
 
 # API modules
 try:
-    from .api.binance_api import BinanceAPI
+    from src.api.binance_api import BinanceAPI
 except ImportError:
     BinanceAPI = None
 
 # Strategy modules
 try:
-    from .strategies.strategy import ScalpingStrategy, SwingTradingStrategy
+    from src.strategies.strategy import ScalpingStrategy, SwingTradingStrategy
 except ImportError:
     ScalpingStrategy = None
     SwingTradingStrategy = None
 
 # Database modules
 try:
-    from .database.db_manager import DBManager
+    from src.database.db_manager import DBManager
 except ImportError:
     DBManager = None
 
 try:
-    from .database.postgres_manager import PostgresManager
+    from src.database.postgres_manager import PostgresManager
 except ImportError:
     PostgresManager = None
 
 # Notification modules
 try:
-    from .notifications.telegram_notifier import TelegramNotifier
+    from src.notifications.telegram_notifier import TelegramNotifier
 except ImportError:
     TelegramNotifier = None
 
 # Trading modules
 try:
-    from .trading.wallet_manager import WalletManager
+    from src.trading.wallet_manager import WalletManager
 except ImportError:
     WalletManager = None
 
 # Risk management modules
 try:
-    from .risk_management.adaptive_risk_manager import AdaptiveRiskManager
+    from src.risk_management.adaptive_risk_manager import AdaptiveRiskManager
 except ImportError:
     AdaptiveRiskManager = None
 
 # Analysis modules
 try:
-    from .analysis.technical_indicators import TechnicalIndicators
+    from src.analysis.technical_indicators import TechnicalIndicators
 except ImportError:
     TechnicalIndicators = None
 
 try:
-    from .analysis.external_data_analyzer import ExternalDataAnalyzer
+    from src.analysis.external_data_analyzer import ExternalDataAnalyzer
 except ImportError:
     ExternalDataAnalyzer = None
 
 # Utilidades
 try:
-    from .utils.utils import (
+    from src.utils.utils import (
         setup_logger, 
         save_state, 
         load_state, 
