a947159eff7566b70dc0c919fc63f9ddff29f00c
agnaldo.marinho.cev <agnaldo.marinho.cev@elo.com.br>
2025-07-07 16:07:39 -0300

    Atualiza src/schemas/__init__.py

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -2,16 +2,16 @@
 Pydantic schemas for Robot-Crypt API.
 """
 
-from .user import UserBase, UserCreate, UserUpdate, UserInDB, User
-from .asset import AssetBase, AssetCreate, AssetUpdate, Asset
-from .technical_indicator import TechnicalIndicatorBase, TechnicalIndicatorCreate, TechnicalIndicator
-from .macro_indicator import MacroIndicatorBase, MacroIndicatorCreate, MacroIndicator
-from .bot_performance import BotPerformanceBase, BotPerformanceCreate, BotPerformance
-from .risk_management import RiskManagementBase, RiskManagementCreate, RiskManagement
-from .alert import AlertBase, AlertCreate, AlertUpdate, Alert
-from .trade import TradeBase, TradeCreate, Trade
-from .report import ReportBase, ReportCreate, Report
-from .token import Token, TokenData
+from src.schemas.user import UserBase, UserCreate, UserUpdate, UserInDB, User
+from src.schemas.asset import AssetBase, AssetCreate, AssetUpdate, Asset
+from src.schemas.technical_indicator import TechnicalIndicatorBase, TechnicalIndicatorCreate, TechnicalIndicator
+from src.schemas.macro_indicator import MacroIndicatorBase, MacroIndicatorCreate, MacroIndicator
+from src.schemas.bot_performance import BotPerformanceBase, BotPerformanceCreate, BotPerformance
+from src.schemas.risk_management import RiskManagementBase, RiskManagementCreate, RiskManagement
+from src.schemas.alert import AlertBase, AlertCreate, AlertUpdate, Alert
+from src.schemas.trade import TradeBase, TradeCreate, Trade
+from src.schemas.report import ReportBase, ReportCreate, Report
+from src.schemas.token import Token, TokenData
 
 __all__ = [
     # User schemas
